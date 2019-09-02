63a36fec65185b715d32a6d52f45c3ade6aca9d5
Aleksandar Dimitrov <aleksandar.dimitrov@itdesign.de>
2019-09-03 00:01:23 +0200

    Fixes & Optics

--- app/src/main/kotlin/Misc.kt
+++ app/src/main/kotlin/Misc.kt
@@ -1,3 +1,6 @@
+
+import arrow.optics.optics
+
 fun <A, B, C> ((B) -> C)
         .compose(f: (A) -> B): (A) -> C = { this(f(it)) }
 
@@ -7,3 +10,15 @@ fun List<String>.parse(): List<Int> = TODO()
 val parseAndSum = List<Int>::sum
     .compose<List<String>, List<Int>, Int>(
         List<String>::parse)
+
+@optics data class Street(
+    val number: Int, val name: String) {companion object}
+@optics data class Address(
+    val city: String, val street: Street) {companion object}
+@optics data class Company(
+    val name: String, val address: Address) {companion object}
+@optics data class Employee(
+    val name: String, val company: Company?) {companion object}
+
+val e : Employee = TODO()
+val f = Employee.company.address.street.name.modify(e) { "$it-Str" }
